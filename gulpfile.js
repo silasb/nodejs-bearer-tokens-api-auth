cf6c4e7c21e41a958acaf234c3ad833245512232
silasb <silas.baronda@gmail.com>
2014-08-04 22:51:05 -0400

    Moving configuration files around

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var browserify = require('gulp-browserify');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('jsx', function() {
   return gulp.src('client/javascripts/app.jsx', {read: false})
@@ -11,8 +12,6 @@ gulp.task('jsx', function() {
     .pipe(rename('app.js'))
     .pipe(gulp.dest('public/javascripts/'));
 });
-
-var nodemon = require('gulp-nodemon');
 
 gulp.task('default', ['jsx'], function() {
   gulp.watch('client/javascripts/**/*.jsx', ['jsx'])
